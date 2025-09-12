c13f30b42d81edff98e3e37183c92ef7ec13d3f4
kiranfin <4finjas@online.de>
2025-09-12 23:23:57 +0200

    Redesigned Spells Tab

--- lib/pages/detail.dart
+++ lib/pages/detail.dart
@@ -787,11 +787,10 @@ class _DetailPageState extends State<DetailPage> {
                   padding: const EdgeInsets.all(10.0),
                   child: Container(
                     decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black),
                       gradient: LinearGradient(
                         begin: Alignment.centerLeft,
                         end: Alignment.centerRight,
-                        colors: [Colors.black.withValues(alpha: 0.8), Colors.black.withValues(alpha: 0.6)],
+                          colors: [Theme.of(context).colorScheme.primary, Theme.of(context).colorScheme.secondary]
                       ),
                       borderRadius: BorderRadius.circular(30),
                     ),
@@ -802,9 +801,7 @@ class _DetailPageState extends State<DetailPage> {
                         children: [
                           Padding(
                             padding: const EdgeInsets.only(left: 8.0, top: 5.0),
-                            child: Text("Spells", style: const TextStyle(
-                                color: Colors.white,
-                                fontSize: 30)
+                            child: Text("Spells", style: Theme.of(context).textTheme.displaySmall!.copyWith(color: Theme.of(context).colorScheme.surface)
                             ),
                           ),
                           Wrap(
@@ -818,9 +815,9 @@ class _DetailPageState extends State<DetailPage> {
                                     borderRadius: BorderRadius.circular(20),
                                     child: Container(
                                       width: 110,
-                                      height: 125,
+                                      height: 130,
                                       decoration: BoxDecoration(
-                                        color: const Color(0xff101010),
+                                        color: Theme.of(context).colorScheme.surfaceContainer,
                                         borderRadius: BorderRadius.circular(20),
                                       ),
                                       child: GridTile(
@@ -831,7 +828,7 @@ class _DetailPageState extends State<DetailPage> {
                                             children: [
                                               AutoSizeText(
                                                   finallist[index]["name"],
-                                                  style: const TextStyle(color: Colors.white, fontSize: 15),
+                                                  style: Theme.of(context).textTheme.labelLarge!.copyWith(color: Theme.of(context).colorScheme.surface),
                                                   maxLines: 1
                                               ),
                                               Row(
@@ -920,7 +917,7 @@ class _DetailPageState extends State<DetailPage> {
                 itemCount: 12,
                 itemBuilder: (BuildContext context, int index) {
                   return ListTile(
-                    title: Shimmer(child: Container(width: 50, height: 15, color: Colors.black45)),
+                    title: ClipRRect(borderRadius: BorderRadius.circular(10), child: Shimmer(color: Theme.of(context).colorScheme.surface, child: SizedBox(width: 50, height: 15))),
                   );
                 }
             );
