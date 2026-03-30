1d45f210cbdc8bcafc61f5453790cfb0bad5aeaa
kiranfin <60066861+kiranfin@users.noreply.github.com>
2026-03-30 19:31:21 +0200

    Add evaluation dependency for subprojects

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -13,6 +13,10 @@ subprojects {
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
 
+subprojects {
+    project.evaluationDependsOn(":app")
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
